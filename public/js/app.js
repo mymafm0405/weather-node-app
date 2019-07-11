a8209eeb24af0360ee389193cd5c749922758aee
mymafm0405m <mymafm0405@gmail.com>
2019-07-11 11:18:37 +0300

    Delete unnessecary codes

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-console.log('Javascript file for the user')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-
-
 const searchForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
